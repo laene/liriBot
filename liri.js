b5f6c4d0d41ccd236e7a4c40c6e41ce26464201a
Elena Henry <laene234@gmail.com>
2018-11-25 10:13:59 -0600

    added do-what-it-says and put each command as its own function

--- liri.js
+++ liri.js
@@ -4,6 +4,8 @@ const axios = require("axios");
 
 const moment = require("moment");
 
+var fs = require("fs");
+
 // var Spotify = require("node-spotify-api");
 
 // var spotifyKeys = require("./keys.js");
@@ -20,6 +22,10 @@ const spotify = new Spotify(keys.spotify);
 //------------------------------------------
 
 let search = "";
+
+let userCommand = process.argv[2];
+
+
 function makeSearch() {
     search = process.argv[3]
     for (let i = 4; i < process.argv.length; i++) {
@@ -29,11 +35,7 @@ function makeSearch() {
 
 //------------------------------------------
 
-if (process.argv[2] === "concert-this") {
-    //Bands in Town API
-    //Takes Artist Name
-    //Gives Event Venue Name, Venue Location, Date
-    makeSearch();
+function findConcerts() {
     axios
         .get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp")
         .then(function (response) {
@@ -51,49 +53,43 @@ if (process.argv[2] === "concert-this") {
         })
         .catch(function (error) {
             if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
                 console.log(error.response.data);
                 console.log(error.response.status);
                 console.log(error.response.headers);
             } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
                 console.log(error.request);
             } else {
-                // Something happened in setting up the request that triggered an Error
                 console.log("Error", error.message);
             }
             console.log(error.config);
         });
+}
+//------------------------------------------
 
-    //------------------------------------------
-
-} else if (process.argv[2] === "spotify-this-song") {
-    //Node-Spotify-API
-    //Takes Song Name
-    //Gives Artist, Song Name, Preview Link on Spotify, Album
-    makeSearch();
-    spotify.search({ type: 'track', query: search }, function(err, data) {
+function findSong() {
+    spotify.search({ type: 'track', query: search }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-    
-        console.log("Song Title: " + data.tracks.items[0].name); 
-        console.log("Album: " + data.tracks.items[0].album.name); 
+
+        var releaseDate = data.tracks.items[0].album.release_date;
+        releaseDate = moment().format("YYYY");
+
+        console.log("Song Title: " + data.tracks.items[0].name);
+        console.log("Album: " + data.tracks.items[0].album.name);
         console.log("Artist: " + data.tracks.items[0].artists[0].name);
-        console.log("Release Year: " + data.tracks.items[0].album.release_date);
+        console.log("Release Year: " + releaseDate);
         console.log("Preview: " + data.tracks.items[0].external_urls.spotify);
-      });
-    //------------------------------------------
+    });
+}
 
-} else if (process.argv[2] === "movie-this") {
-    //OMDB API (trilogy)
-    //Takes Movie Name
-    //Gives Title, Year, IMDB Rating, RT Rating, Country, Language, Actors, Plot
-    //Default "Mr. Nobody"
+//------------------------------------------
 
-    makeSearch();
+function findMovie() {
+    if (search === undefined) {
+        search = "mr nobody";
+        console.log("Have you seen Mr. Nobody? You should!");
+    };
     axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy")
         .then(function (response) {
             console.log(
@@ -110,13 +106,122 @@ Plot: ${response.data.Plot}
             );
         });
 
+
+}
+
 //------------------------------------------
 
-} else if (process.argv[2] === "do-what-it-says") {
-    //Spotify-This-Song 
-    //Use the text in the random.txt file though
-    makeSearch();
-} else {
-    console.log("That is not a valid command. Have a nice day!")
+function liriBotTime() {
+    if (userCommand === "concert-this") {
+        //Bands in Town API
+        //Takes Artist Name
+        //Gives Event Venue Name, Venue Location, Date
+
+        makeSearch();
+        findConcerts();
+
+        //------------------------------------------
+
+    } else if (userCommand === "spotify-this-song") {
+        //Node-Spotify-API
+        //Takes Song Name
+        //Gives Artist, Song Name, Preview Link on Spotify, Album
+        makeSearch();
+        findSong();
+
+        //------------------------------------------
+
+    } else if (userCommand === "movie-this") {
+        //OMDB API (trilogy)
+        //Takes Movie Name
+        //Gives Title, Year, IMDB Rating, RT Rating, Country, Language, Actors, Plot
+        //Default "Mr. Nobody"
+
+        makeSearch();
+        findMovie();
+
+        //------------------------------------------
+
+    } else if (userCommand === "do-what-it-says") {
+        //Spotify-This-Song 
+        //Use the text in the random.txt file though
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                return console.log(error);
+            }
+
+            var dataArr = data.split(",");
+            console.log(dataArr);
+
+            userCommand = dataArr[0];
+            search = dataArr[1];
+            console.log(userCommand);
+            console.log(search);
+            
+            if (userCommand === "spotify-this-song") {
+                findSong();
+            }
+
+        });
+
+    } else {
+        console.log("That is not a valid command. Have a nice day!")
+    }
 }
-//------------------------------------------
+
+//-----------------------------------------
+
+liriBotTime();
+
+// if (userCommand === "concert-this") {
+//     //Bands in Town API
+//     //Takes Artist Name
+//     //Gives Event Venue Name, Venue Location, Date
+
+//     makeSearch();
+//     findConcerts();
+
+//     //------------------------------------------
+
+// } else if (userCommand === "spotify-this-song") {
+//     //Node-Spotify-API
+//     //Takes Song Name
+//     //Gives Artist, Song Name, Preview Link on Spotify, Album
+//     makeSearch();
+//     findSong();
+
+//     //------------------------------------------
+
+// } else if (userCommand === "movie-this") {
+//     //OMDB API (trilogy)
+//     //Takes Movie Name
+//     //Gives Title, Year, IMDB Rating, RT Rating, Country, Language, Actors, Plot
+//     //Default "Mr. Nobody"
+
+//     makeSearch();
+//     findMovie();
+
+//     //------------------------------------------
+
+// } else if (userCommand === "do-what-it-says") {
+//     //Spotify-This-Song 
+//     //Use the text in the random.txt file though
+//     fs.readFile("random.txt", "utf8", function (error, data) {
+//         if (error) {
+//             return console.log(error);
+//         }
+
+//         var dataArr = data.split(",");
+//         console.log(dataArr);
+
+//         userCommand = dataArr[0];
+//         search = dataArr[1];
+//         console.log(userCommand);
+//         console.log(search);
+
+//     });
+
+// } else {
+//     console.log("That is not a valid command. Have a nice day!")
+// }
+// //------------------------------------------
